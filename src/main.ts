300fb477f36fca41eabeb8e986d5d59eca38a786
jthornburgllc <jeff@jthornburg.com>
2018-06-12 22:32:43 -0400

    Cleanup2
    Cleanup2

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { DataTablesModule } from 'angular-datatables';
-
-import { AppComponent } from './app/app.component';
-
-
 if (environment.production) {
   enableProdMode();
 }
